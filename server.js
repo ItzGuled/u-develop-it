d3595bd905c953b898ddec7b790b7c2854d2f13e
itzguled <itzguled55337@gmail.com>
2022-03-10 17:29:09 -0600

    join tables

--- server.js
+++ server.js
@@ -23,8 +23,11 @@ const db = mysql.createConnection(
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
-
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
   db.query(sql, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
@@ -39,7 +42,12 @@ app.get("/api/candidates", (req, res) => {
 
 // Get a single candidate
 app.get("/api/candidate/:id", (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -90,18 +98,18 @@ app.post("/api/candidate", ({ body }, res) => {
   }
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
   VALUES (?,?,?)`;
-const params = [body.first_name, body.last_name, body.industry_connected];
+  const params = [body.first_name, body.last_name, body.industry_connected];
 
-db.query(sql, params, (err, result) => {
-  if (err) {
-    res.status(400).json({ error: err.message });
-    return;
-  }
-  res.json({
-    message: 'success',
-    data: body
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
   });
-});
 });
 
 // Default response for any other request (Not Found)
